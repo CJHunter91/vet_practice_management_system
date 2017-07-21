91e973bd2a3489fe0dc84dd0fc49b0484ee61293
Chris Hunter <orkem2@gmail.com>
2017-07-21 13:47:51 +0100

    Added name attribute to pet

--- models/pet.rb
+++ models/pet.rb
@@ -3,9 +3,12 @@ class Pet
 
   def initialize(details)
     @id = details['id'] if details['id']
+    @name = details['name']
     @age = details['age']
     @type = details['type']
     @breed = details['breed']
     @owner_id = details['owner_id']
   end
+
+
 end

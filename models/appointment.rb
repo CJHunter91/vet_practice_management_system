3146d433437686180ea12583dfb11d1e7d2c8482
Chris Hunter <orkem2@gmail.com>
2017-07-21 14:12:28 +0100

    Added attr_readers to appointment class.

--- models/appointment.rb
+++ models/appointment.rb
@@ -1,6 +1,6 @@
   
 class Appointment
-
+  attr_reader :id, :time, :duration, :needs_seen, :pet_id
   def initialize(details)
     @id = details['id'] if details['id']
     @time = details['time']
@@ -8,5 +8,6 @@ class Appointment
     @needs_seen = details['needs_seen']
     @pet_id = details['pet_id']
   end
-  
+
+
 end

47bd4d768a27078f5ebe05ddbaf4668a93cf12ad
Chris Hunter <orkem2@gmail.com>
2017-07-21 13:11:13 +0100

    Added owners table and columns to vet_practice_tables.sql

--- db/vet_practice_tables.sql
+++ db/vet_practice_tables.sql
@@ -1,1 +1,18 @@
+DROP TABLE IF EXISTS appointments;
+DROP TABLE IF EXISTS pets;
+DROP TABLE IF EXISTS owners;
 
+CREATE TABLE owners(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  phone VARCHAR(255)
+);
+
+CREATE TABLE pets(
+
+);
+
+CREATE TABLE appointments(
+
+);

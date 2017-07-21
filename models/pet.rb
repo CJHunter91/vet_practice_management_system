bab33afb47b364799095783680eb66b5ddf76c29
Chris Hunter <orkem2@gmail.com>
2017-07-21 13:25:07 +0100

    Added initialize to pet class

---
+++ models/pet.rb
@@ -0,0 +1,11 @@
+
+class Pet
+
+  def initialize(details)
+    @id = details['id'] if details['id']
+    @age = details['age']
+    @type = details['type']
+    @breed = details['breed']
+    @owner_id = details['owner_id']
+  end
+end

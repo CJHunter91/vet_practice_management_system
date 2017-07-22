1fa8dcfec7388ee4fd870ba467878f5343fef3d2
Chris Hunter <orkem2@gmail.com>
2017-07-22 16:13:53 +0100

    added form to appointments which uses get to change the owner index page so that a owner can be selected for a new appointment

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -5,16 +5,16 @@ get '/owners' do
   erb(:'owners/index')
 end
 
-get '/owners/:id' do 
-  @owner = Owner.find(params[:id])
-  @pets = @owner.get_pets
-  erb(:'owners/show')
-end
-
 get '/owners/new' do 
   erb(:'owners/new')
 end
 
 post '/owners' do 
   Owner.new(params).save
+end
+
+get '/owners/:id' do 
+  @owner = Owner.find(params[:id])
+  @pets = @owner.get_pets
+  erb(:'owners/show')
 end

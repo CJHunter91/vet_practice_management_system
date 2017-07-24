267d110f37e4d40e3a5c8988530fc1c26f520220
Chris Hunter <orkem2@gmail.com>
2017-07-24 09:17:56 +0100

    Added new pet route for showing pet

--- controllers/pet_controller.rb
+++ controllers/pet_controller.rb
@@ -4,6 +4,12 @@ get '/pet/:id/new' do
   erb(:"pets/new")
 end
 
+get 'pet/:owner_id/:pet_id' do
+  @pet = params['pet_id']
+  @owner = @pet.get_owner
+  erb(:"pets/show")
+end
+
 post '/pet' do 
   Pet.new( params ).save
   redirect to '/owners/' + params[:owner_id].to_s

67196a058996f58a54935b8e11ca741085d6868b
Chris Hunter <orkem2@gmail.com>
2017-07-24 10:29:55 +0100

    Added edit route for pet and created edit form

--- controllers/pet_controller.rb
+++ controllers/pet_controller.rb
@@ -1,7 +1,7 @@
 require_relative('../models/pet')
 
-get '/pet/:pet_id' do
-  @pet = Pet.find(params[:pet_id])
+get '/pet/:id' do
+  @pet = Pet.find(params[:id])
   @owner = @pet.get_owner
   erb(:"pets/show")
 end
@@ -10,9 +10,12 @@ get '/pet/:id/new' do
   erb(:"pets/new")
 end
 
-
-
 post '/pet' do 
   Pet.new( params ).save
   redirect to '/owners/' + params[:owner_id].to_s
+end
+
+get '/pet/:id/edit' do
+  @pet = Pet.find(params[:id])
+  erb(:"pets/edit")
 end
